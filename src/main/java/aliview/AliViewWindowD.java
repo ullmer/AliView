034843ef9a29836a0d035fc1faea74d3f369d4bb
Brygg Ullmer <ullmer@acm.org>
2024-08-05 20:50:49 -0400

    progress

--- src/main/java/aliview/AliViewWindowD.java
+++ src/main/java/aliview/AliViewWindowD.java
@@ -3,26 +3,29 @@ package aliview;
 public class AliViewWindowD extends AliViewWindow {
 
 
+  public AliViewWindow(File alignmentFile, AliViewJMenuBarFactory menuBarFactory) {
+        super(alignmentFile, menuBarFactory);
+  }
 
+  public void zoomInAt(Point mousePos){
+    super.zoomInAt(mousePos);
+  }
+/*
+  public void moveSelectedToBottom() {
+    aliViewWindow.getUndoControler().pushUndoState(new UndoSavedStateSequenceOrder(alignment.getSequences().getDelegateSequencesCopy(), alignment.getAlignentMetaCopy()));
+    alignment.moveSelectedSequencesToBottom();
+    //requestPaneAndListRepaint();
+    //alignmentPane.validateSize();
+  }
 
-public void zoomInAt(Point mousePos){
-  super.zoomInAt(mousePos);
+  public void moveSelectedToTop() {
+    aliViewWindow.getUndoControler().pushUndoState(new UndoSavedStateSequenceOrder(alignment.getSequences().getDelegateSequencesCopy(), alignment.getAlignentMetaCopy()));
+    alignment.moveSelectedSequencesToTop();
+    //requestPaneAndListRepaint();
+    //alignmentPane.validateSize();
+  }
+
+  //public void moveSelectedDown() 
+*/
 }
-
-}
-public void moveSelectedToBottom() {
-		aliViewWindow.getUndoControler().pushUndoState(new UndoSavedStateSequenceOrder(alignment.getSequences().getDelegateSequencesCopy(), alignment.getAlignentMetaCopy()));
-		alignment.moveSelectedSequencesToBottom();
-		//requestPaneAndListRepaint();
-		//alignmentPane.validateSize();
-	}
-
-	public void moveSelectedToTop() {
-		aliViewWindow.getUndoControler().pushUndoState(new UndoSavedStateSequenceOrder(alignment.getSequences().getDelegateSequencesCopy(), alignment.getAlignentMetaCopy()));
-		alignment.moveSelectedSequencesToTop();
-		//requestPaneAndListRepaint();
-		//alignmentPane.validateSize();
-	}
-
-	public void moveSelectedDown() 
 /// end ///
