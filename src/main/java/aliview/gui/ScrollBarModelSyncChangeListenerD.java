f3f609ace29e8fd589852aab00fa2449ce335e0f
Brygg Ullmer <ullmer@acm.org>
2024-08-07 22:45:31 -0400

    event intercept

---
+++ src/main/java/aliview/gui/ScrollBarModelSyncChangeListenerD.java
@@ -0,0 +1,19 @@
+package aliview.gui;
+
+
+
+/**
+ *        Synchronize the data models of any two JComponents that use a
+ *         BoundedRangeModel ( such as a JScrollBar, JSlider or ProgressBar).
+ *
+ *        @see javax.swing.BoundedRangeModel
+ *        @see javax.swing.event.ChangeListener
+ *        @Author  R. Kevin Cole        
+ */
+public class ScrollBarModelSyncChangeListenerD extends ScrollBarModelSyncChangeListener
+{
+   public void stateChanged( ChangeEvent e ){
+      System.out.println("ScrollBarModelSyncChangeListenerD stateChanged:" + e);
+      super.stateChanged(e);
+   }
+}
